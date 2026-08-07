37269881f66ade3baf1087e82b1fb0b7a80d17d4
Jit-Roy <royjit0506@gmail.com>
2026-08-07 06:28:01 +0530

    Bug Fixes

--- weellm/__init__.py
+++ weellm/__init__.py
@@ -1,5 +1,4 @@
 __version__ = "0.1.0"
-from .pipeline import WeePipeline
 from .diffusion_pipeline import DiffusionPipeline
 
-__all__ = ["WeePipeline", "DiffusionPipeline"]
+__all__ = ["DiffusionPipeline"]
